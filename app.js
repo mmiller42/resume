d4bef38f932ce1741864ccd24b546232c1e1c54e
Matt Miller <mmiller@helixeducation.com>
2015-12-28 12:18:21 -0700

    Enforce HTTPS

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 var express = require('express');
 var app = express();
 
+app.use(function (req, res, next) {
+	if (!req.secure) {
+		return res.redirect('https://' + req.headers.host + req.url);
+	}
+
+	next();
+});
+
 app.use(express.static('assets'));
 
 app.listen(process.env.PORT || 3000);
